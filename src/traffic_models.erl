85777f6a53c1ff48b5ae3aa49180a94fe7c3796a
leonardofl <leonardofl87@gmail.com>
2020-02-18 11:46:02 -0300

    Codando class_Bike

--- src/traffic_models.erl
+++ src/traffic_models.erl
@@ -1,6 +1,6 @@
 -module(traffic_models).
 
--export([get_speed_car/2, get_speed_walk/2]).
+-export([get_speed_car/2, get_speed_walk/2, get_speed_bike/0]).
 
 % There is DR in link and car can use it:
 get_speed_car({_, Id, Length, RawCapacity, Freespeed, NumberCars, Lanes, {_DRName, _DigitalRailsLanes, _Cycle, _Bandwidth, _Signalized, _Offset}}, true) ->
@@ -30,8 +30,17 @@ link_density_speed(Id, Length, Capacity, NumberCars, Freespeed, _Lanes) ->
 	Time = (Length / Speed) + 1,
 	{Id, round(Time), round(Length)}.
 
+
+
+
 get_speed_walk(LinkData, _) ->
 	{_, Id, Length, _, _, _} = LinkData,	
 	Time = ( Length / 2 ),
 
 	{Id, ceil(Time), round(Length)}.
+
+
+
+get_speed_bike() ->
+    14/3.6.
+
